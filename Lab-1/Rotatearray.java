542127504792881cf0c1f099aafd7435098d74e3
Siddharth Bhat <siddharthbhat99@gmail.com>
2025-02-24 13:42:58 +0530

    Add files via upload

---
+++ Lab-1/Rotatearray.java
@@ -0,0 +1,10 @@
+package Lab_1;
+
+public class Rotatearray {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Lab-1/typecasting.java
@@ -0,0 +1,18 @@
+package Lab_1;
+
+public class typecasting {
+
+	public static void main(String[] args) {
+		int x1=10;
+		int y1=(int)x1;
+		System.out.print(y1);
+		//double x2=10.5;
+		//int y2=x2;
+		//System.out.print(y2);
+		double x3=10.5;
+		double y3=x3;
+		System.out.print(y3);
+
+	}
+
+}
